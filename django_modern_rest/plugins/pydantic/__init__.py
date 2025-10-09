7b4dad70741c0f248329725bb602bacba1038366
sobolevn <mail@sobolevn.me>
2025-10-09 12:47:11 +0300

    Fix CI

--- django_modern_rest/plugins/pydantic/__init__.py
+++ django_modern_rest/plugins/pydantic/__init__.py
@@ -2,8 +2,8 @@ from typing import final
 
 try:
     import pydantic
-except ImportError:
-    print(
+except ImportError:  # pragma: no cover
+    print(  # noqa: WPS421
         'Looks like `pydantic` is not installed, '
         "consider using `pip install 'django-modern-rest[pydantic]'`",
     )
