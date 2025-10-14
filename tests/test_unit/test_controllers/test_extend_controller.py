275e12d6ff49caa1d7b95ed4d984049d364d49d8
sobolevn <mail@sobolevn.me>
2025-10-14 16:59:23 +0300

    Validate that controllers are either sync or async (#35)

--- tests/test_unit/test_controllers/test_extend_controller.py
+++ tests/test_unit/test_controllers/test_extend_controller.py
@@ -4,6 +4,7 @@ import pytest
 
 from django_modern_rest import Controller
 from django_modern_rest.exceptions import UnsolvableAnnotationsError
+from django_modern_rest.plugins.pydantic import PydanticSerializer
 from django_modern_rest.serialization import BaseSerializer
 
 _SerializerT = TypeVar('_SerializerT', bound=BaseSerializer)
@@ -13,17 +14,27 @@ def test_controller_without_serializer() -> None:
     """Ensure that we need at least one type param for component."""
     with pytest.raises(UnsolvableAnnotationsError, match='_Custom'):
 
-        class _Custom(Controller): ...  # type: ignore[type-arg]  # noqa: WPS431, WPS604
+        class _Custom(Controller):  # type: ignore[type-arg]
+            """Empty."""
 
 
 def test_controller_generic_subclass() -> None:
     """Ensure that we can extend controllers with generics."""
 
-    class _Custom(Controller[_SerializerT]): ...  # noqa: WPS431, WPS604
+    class _Custom(Controller[_SerializerT]):
+        """Empty."""
 
 
 def test_controller_wrong_serializer() -> None:
     """Ensure that we must pass BaseSerializer types to controllers."""
     with pytest.raises(UnsolvableAnnotationsError, match='BaseSerializer'):
 
-        class _Custom(Controller[int]): ...  # type: ignore[type-var]  # noqa: WPS431, WPS604
+        class _Custom(Controller[int]):  # type: ignore[type-var]
+            """Empty."""
+
+
+def test_controller_empty() -> None:
+    """Ensure that we can create empty controllers."""
+
+    class _Custom(Controller[PydanticSerializer]):
+        """Empty."""
