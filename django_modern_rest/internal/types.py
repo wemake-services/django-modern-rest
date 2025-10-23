8a6c75dc85a4d2dc2c4774b08b0b0213c161e69a
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-10-23 20:54:33 +0100

    Added type dict for settings defaults (#143)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: sobolevn <mail@sobolevn.me>

---
+++ django_modern_rest/internal/types.py
@@ -0,0 +1,22 @@
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any, TypedDict
+
+from django_modern_rest.internal.json import Deserialize, Serialize
+
+if TYPE_CHECKING:
+    from django_modern_rest.controller import Controller
+    from django_modern_rest.endpoint import Endpoint
+    from django_modern_rest.openapi.config import OpenAPIConfig
+
+
+class DMRSettings(TypedDict, total=False):
+    """TypedDict defining the shape of our settings."""
+
+    serialize: str | Serialize
+    deserialize: str | Deserialize
+    openapi_config: 'OpenAPIConfig'
+    validate_responses: bool
+    responses: list[Any]
+    global_error_handler: (
+        str | Callable[['Controller[Any]', 'Endpoint', Exception], Any]
+    )
--- django_modern_rest/types.py
+++ django_modern_rest/types.py
@@ -1,10 +1,17 @@
 import dataclasses
 from collections.abc import Callable
-from typing import Any, Final, final, get_args, get_origin
+from typing import (
+    Any,
+    Final,
+    final,
+    get_args,
+    get_origin,
+)
 
 from typing_extensions import get_original_bases, get_type_hints
 
 from django_modern_rest.exceptions import UnsolvableAnnotationsError
+from django_modern_rest.internal.types import DMRSettings as DMRSettings
 
 
 @final
@@ -63,8 +70,8 @@ def parse_return_annotation(endpoint_func: Callable[..., Any]) -> Any:
         endpoint_func: function with return type annotation.
 
     Raises:
-        UnsolvableAnnotationsError: when annotation can't be solved.
-            Or when does not exist.
+        UnsolvableAnnotationsError: when annotation can't be solved
+            or when the annotation does not exist.
 
     Returns:
         Function's parsed and solved return type.
@@ -87,8 +94,7 @@ def parse_return_annotation(endpoint_func: Callable[..., Any]) -> Any:
 
 
 def is_safe_subclass(annotation: Any, base_class: type[Any]) -> bool:
-    """Possibly unwraps subscribbed class before checking for subclassing."""
-    # Not a type guard, because `annotation` can be not a `type` :(
+    """Possibly unwraps subscribed class before checking for subclassing."""
     if annotation is None:
         annotation = type(None)
     try:
