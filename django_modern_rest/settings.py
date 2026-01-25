338ef8fc9da35c6d19e46ee7817090e4bcf76566
sobolevn <mail@sobolevn.me>
2026-01-25 12:02:27 +0300

    Clear all caches

--- django_modern_rest/settings.py
+++ django_modern_rest/settings.py
@@ -6,6 +6,9 @@ from typing import Any, Final, final
 from django.utils import module_loading
 
 from django_modern_rest.envs import MAX_CACHE_SIZE
+from django_modern_rest.internal.cache import (
+    clear_settings_cache as clear_settings_cache,
+)
 from django_modern_rest.openapi.config import OpenAPIConfig
 
 try:
@@ -141,46 +144,3 @@ def resolve_setting(
                 for list_item in setting  # pyright: ignore[reportUnknownVariableType]
             ]
     return setting  # pyright: ignore[reportUnknownVariableType]
-
-
-def clear_settings_cache() -> None:  # noqa: C901
-    """
-    Clears settings cache for all functions in this module.
-
-    Useful for tests, when you modify the global settings object.
-    """
-    try:
-        from django_modern_rest.plugins.pydantic import (  # noqa: PLC0415
-            _get_cached_type_adapter,  # pyright: ignore[reportPrivateUsage]
-        )
-    except ImportError:  # pragma: no cover
-        pass  # noqa: WPS420
-    else:
-        _get_cached_type_adapter.cache_clear()
-
-    try:
-        from django_modern_rest.plugins.msgspec.json import (  # noqa: PLC0415
-            _get_serializer,  # pyright: ignore[reportPrivateUsage]
-        )
-    except ImportError:  # pragma: no cover
-        pass  # noqa: WPS420
-    else:
-        _get_serializer.cache_clear()
-
-    try:
-        from django_modern_rest.plugins.msgspec.json import (  # noqa: PLC0415
-            _get_deserializer,  # pyright: ignore[reportPrivateUsage]
-        )
-    except ImportError:  # pragma: no cover
-        pass  # noqa: WPS420
-    else:
-        _get_deserializer.cache_clear()
-
-    from django_modern_rest.validation.response import (  # noqa: PLC0415
-        _is_validation_enabled,  # pyright: ignore[reportPrivateUsage]
-    )
-
-    _is_validation_enabled.cache_clear()
-
-    _resolve_defaults.cache_clear()
-    resolve_setting.cache_clear()
