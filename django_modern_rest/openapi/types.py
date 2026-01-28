80314467fff1d3879a63743de1008a61fd10e6b7
milssky <2furius@gmail.com>
2026-01-28 15:10:26 +0300

    remove fields

--- django_modern_rest/openapi/types.py
+++ django_modern_rest/openapi/types.py
@@ -5,38 +5,36 @@ from collections.abc import Sequence
 from dataclasses import dataclass, field
 from typing import Any
 
-from django_modern_rest.types import Empty, EmptyObj
 
-
-@dataclass(frozen=True)
+@dataclass(slots=True, frozen=True)
 class KwargDefinition:
     """Data container representing a constrained kwarg."""
 
     # TODO: use type/dataclass for examples
-    examples: list[dict[str, Any]] | None = field(default=None)
+    examples: list[dict[str, Any]] | None = None
     # TODO: use type/dataclass for external docs
-    external_docs: dict[str, Any] | None = field(default=None)
-    content_encoding: str | None = field(default=None)
-    default: Any = field(default=Empty)
-    title: str | None = field(default=None)
-    description: str | None = field(default=None)
-    const: bool | None = field(default=None)
-    gt: float | None = field(default=None)
-    ge: float | None = field(default=None)
-    lt: float | None = field(default=None)
-    le: float | None = field(default=None)
-    multiple_of: float | None = field(default=None)
-    min_items: int | None = field(default=None)
-    max_items: int | None = field(default=None)
-    min_length: int | None = field(default=None)
-    max_length: int | None = field(default=None)
-    pattern: str | None = field(default=None)
-    lower_case: bool | None = field(default=None)
-    upper_case: bool | None = field(default=None)
-    format: str | None = field(default=None)
-    enum: Sequence[Any] | None = field(default=None)
-    read_only: bool | None = field(default=None)
-    schema_extra: dict[str, Any] | None = field(default=None)
+    external_docs: dict[str, Any] | None = None
+    content_encoding: str | None = None
+    default: Any = None
+    title: str | None = None
+    description: str | None = None
+    const: bool | None = None
+    gt: float | None = None
+    ge: float | None = None
+    lt: float | None = None
+    le: float | None = None
+    multiple_of: float | None = None
+    min_items: int | None = None
+    max_items: int | None = None
+    min_length: int | None = None
+    max_length: int | None = None
+    pattern: str | None = None
+    lower_case: bool | None = None
+    upper_case: bool | None = None
+    format: str | None = None
+    enum: Sequence[Any] | None = None
+    read_only: bool | None = None
+    schema_extra: dict[str, Any] | None = None
     schema_component_key: str | None = None
     include_in_schema: bool = True
 
@@ -46,6 +44,6 @@ class FieldDefinition:
     """Definition of a field in the OpenAPI."""
 
     name: str
-    default: Any | Empty = EmptyObj
+    default: Any = None
     extra_data: dict[str, Any] = field(default_factory=dict)  # pyright: ignore[reportUnknownVariableType]
-    kwarg_definition: KwargDefinition | None = field(default=None)
+    kwarg_definition: KwargDefinition | None = None
