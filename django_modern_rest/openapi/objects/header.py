9d463af4195f5aec7f2efa224b50b6a73f3101db
milssky <2furius@gmail.com>
2026-02-17 16:21:44 +0300

    Fix docstring

--- django_modern_rest/openapi/objects/header.py
+++ django_modern_rest/openapi/objects/header.py
@@ -16,12 +16,8 @@ class Header:
 
     The Header Object follows the structure of the Parameter Object
     with the following changes:
-
-    1. `name` MUST NOT be specified, it is given in the corresponding
-        headers map.
-    2. `in` MUST NOT be specified, it is implicitly in header.
-    3. All traits that are affected by the location MUST be applicable to
-        a location of header (for example, style).
+    All traits that are affected by the location MUST be applicable to
+    a location of header (for example, style).
     """
 
     schema: 'Schema | Reference | None' = None
