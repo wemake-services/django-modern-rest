4cdefdcf8b7e01986def4f756daa0406dc0e44a5
Uladzislaw <uladzislawchyrets@gmail.com>
2026-02-28 20:20:12 +0300

    remove django stubs ext

--- dmr/security/blocklist/models.py
+++ dmr/security/blocklist/models.py
@@ -10,21 +10,21 @@ from dmr.security.blocklist.apps import BlocklistConfig
 class BlocklistedJWTToken(models.Model):
     """Model for Blocklisted token."""
 
-    user: models.ForeignKey[Any, Any] = models.ForeignKey(
+    user: 'models.ForeignKey[Any, Any]' = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
     )
-    jti: models.CharField[Any, Any] = models.CharField(
+    jti: 'models.CharField[Any, Any]' = models.CharField(
         unique=True,
         max_length=255,
     )
-    expires_at: models.DateTimeField[Any, Any] = models.DateTimeField()
-    created_at: models.DateTimeField[Any, Any] = models.DateTimeField(
+    expires_at: 'models.DateTimeField[Any, Any]' = models.DateTimeField()
+    created_at: 'models.DateTimeField[Any, Any]' = models.DateTimeField(
         auto_now_add=True,
     )
-    updated_at: models.DateTimeField[Any, Any] = models.DateTimeField(
+    updated_at: 'models.DateTimeField[Any, Any]' = models.DateTimeField(
         auto_now=True,
     )
 
