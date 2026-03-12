43f56692793c424e2538a939767118b86c699dfa
Asiman <a.gasanov@start.ru>
2026-03-12 13:10:48 +0500

    Make an @overloaded function from load_schema

--- dmr/openapi/mappers/schema_loader.py
+++ dmr/openapi/mappers/schema_loader.py
@@ -24,83 +24,14 @@ def load_schema(
     *,
     annotation: Any | Empty = EmptyObj,
     serializer: type['BaseSerializer'] | None = None,
-) -> Schema:
-    """
-    Load schema from python's dict into a dataclass.
-
-    Sadly, we can't use ``serializer.from_python`` until
-    this problem with ``msgspec`` is fixed:
-    https://github.com/jcrist/msgspec/issues/982
-
-    After that we will just use the serializer and remove this code.
-    """
-    examples = raw_data.get('examples')
-    assert serializer is not None, 'serializer instance is required'  # noqa: S101
-    example = generate_example(annotation, serializer)
-
-    return Schema(
-        all_of=_try_sequence(raw_data.get('allOf')),
-        any_of=_try_sequence(raw_data.get('anyOf')),
-        one_of=_try_sequence(raw_data.get('oneOf')),
-        schema_not=_try_optional_type(raw_data.get('not')),
-        schema_if=_try_optional_type(raw_data.get('if')),
-        then=_try_optional_type(raw_data.get('then')),
-        schema_else=_try_optional_type(raw_data.get('else')),
-        dependent_schemas=_try_dict(raw_data.get('dependentSchemas')),
-        prefix_items=_try_sequence(raw_data.get('prefixItems')),
-        items=_try_optional_bool_type(raw_data.get('items')),
-        contains=_try_optional_type(raw_data.get('contains')),
-        properties=_try_dict(raw_data.get('properties')),
-        pattern_properties=_try_dict(raw_data.get('patternProperties')),
-        additional_properties=_try_additional_properties(
-            raw_data.get('additionalProperties'),
-        ),
-        property_names=_try_optional_type(raw_data.get('propertyNames')),
-        unevaluated_items=_try_optional_type(raw_data.get('unevaluatedItems')),
-        unevaluated_properties=_try_optional_type(
-            raw_data.get('unevaluatedProperties'),
-        ),
-        type=_try_type_field(raw_data.get('type')),
-        enum=raw_data.get('enum'),
-        const=raw_data.get('const'),
-        multiple_of=raw_data.get('multipleOf'),
-        maximum=raw_data.get('maximum'),
-        exclusive_maximum=raw_data.get('exclusiveMaximum'),
-        minimum=raw_data.get('minimum'),
-        exclusive_minimum=raw_data.get('exclusiveMinimum'),
-        max_length=raw_data.get('maxLength'),
-        min_length=raw_data.get('minLength'),
-        pattern=raw_data.get('pattern'),
-        max_items=raw_data.get('maxItems'),
-        min_items=raw_data.get('minItems'),
-        unique_items=raw_data.get('uniqueItems'),
-        max_contains=raw_data.get('maxContains'),
-        min_contains=raw_data.get('minContains'),
-        max_properties=raw_data.get('maxProperties'),
-        min_properties=raw_data.get('minProperties'),
-        required=raw_data.get('required', []),
-        dependent_required=raw_data.get('dependentRequired'),
-        format=_try_enum(OpenAPIFormat, raw_data.get('format')),
-        content_encoding=raw_data.get('contentEncoding'),
-        content_media_type=raw_data.get('contentMediaType'),
-        content_schema=_try_optional_type(raw_data.get('contentSchema')),
-        title=raw_data.get('title'),
-        description=raw_data.get('description'),
-        default=raw_data.get('default'),
-        deprecated=raw_data.get('deprecated'),
-        read_only=raw_data.get('readOnly'),
-        write_only=raw_data.get('writeOnly'),
-        discriminator=_try_discriminator(raw_data.get('discriminator')),
-        xml=_try_xml(raw_data.get('xml')),
-        external_docs=_try_external_documentation(raw_data.get('externalDocs')),
-        examples=examples,
-        example=example,
-    )
+) -> Schema: ...
 
 
-@overload
 def load_schema(
     raw_data: dict[str, Any],
+    *,
+    annotation: Any | Empty,
+    serializer: type['BaseSerializer'] | None,
 ) -> Schema:
     """
     Load schema from python's dict into a dataclass.
@@ -113,6 +44,10 @@ def load_schema(
     """
     examples = raw_data.get('examples')
     example = raw_data.get('example')
+
+    if not example and not examples:
+        assert serializer is not None, 'serializer instance is required'  # noqa: S101
+        example = generate_example(annotation, serializer)
 
     return Schema(
         all_of=_try_sequence(raw_data.get('allOf')),
