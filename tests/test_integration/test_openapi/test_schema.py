4d99b9d98a5229d458788f0de9c5b04bfa8b9bd0
Alexander Kondratev <kondratev.av@inbox.ru>
2026-03-14 13:49:34 +0500

    Connect tracecov to schemathesis

--- tests/test_integration/test_openapi/test_schema.py
+++ tests/test_integration/test_openapi/test_schema.py
@@ -1,12 +1,34 @@
+from collections.abc import Iterator
+
 import pytest
 import schemathesis as st
+import tracecov  # type: ignore[import-untyped]
 from django.urls import reverse
 from schemathesis.specs.openapi.schemas import OpenApiSchema
+from tracecov.schemathesis import helpers  # type: ignore[import-untyped]
 
 from django_test_app.server.wsgi import application
 
 
-# NOTE: The `db` fixture is required to enable database access.
+@pytest.fixture(scope='session')
+def coverage_map() -> Iterator[tracecov.CoverageMap]:
+    """
+    Provide a ``Tracecov`` coverage map for the whole test session.
+
+    The coverage map is initialized from the current schema and is used
+    during tests to record which API operations and responses are
+    exercised by the test suite. After the tests finishes, a coverage
+    report is generated and saved to ``tracecov.json``.
+    """
+    from django_test_app.server.urls import schema  # noqa: PLC0415
+
+    coverage_map = tracecov.CoverageMap.from_dict(schema.convert())
+    yield coverage_map
+
+    coverage_map.save_report(output_file='tracecov.json', format='json')
+
+
+# The `db` fixture is required to enable database access.
 # When `st.openapi.from_wsgi()` makes a WSGI request, Django's request
 # lifecycle triggers database operations.
 @pytest.fixture
@@ -19,6 +41,15 @@ schema = st.pytest.from_fixture('api_schema')
 
 
 @schema.parametrize()
-def test_schemathesis(case: st.Case) -> None:
+def test_schemathesis(
+    case: st.Case,
+    coverage_map: tracecov.CoverageMap,
+) -> None:
     """Ensure that API implementation matches the OpenAPI schema."""
-    case.call_and_validate()
+    response = case.call_and_validate()
+    # Record interaction for `tracecov` report:
+    coverage_map.record_schemathesis_interactions(
+        case.method,
+        case.operation.full_path,
+        [helpers.from_response(case.method, response)],
+    )
