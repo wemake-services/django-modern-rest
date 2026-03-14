3fdf2ee97a859b2c1c213e0f6c5108a15a309f8c
sobolevn <mail@sobolevn.me>
2026-03-14 16:45:02 +0300

    Improve `build_404_handler` docs (#648)

---
+++ tests/test_integration/test_not_found_view.py
@@ -0,0 +1,25 @@
+from http import HTTPMethod, HTTPStatus
+
+import pytest
+from inline_snapshot import snapshot
+
+from dmr.test import DMRClient
+
+
+@pytest.mark.parametrize('method', list(HTTPMethod))
+def test_not_found_view(
+    dmr_client: DMRClient,
+    *,
+    method: HTTPMethod,
+) -> None:
+    """Test that 404 view works."""
+    response = dmr_client.generic(str(method), '/api/missing')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.headers['Content-Type'] == 'application/json'
+    if method == HTTPMethod.HEAD:
+        assert not response.content
+    else:
+        assert response.json() == snapshot({
+            'detail': [{'msg': 'Page not found', 'type': 'not_found'}],
+        })
