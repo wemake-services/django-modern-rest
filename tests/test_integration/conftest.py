8f1a13c1c3df3013f9cbb395942cad8847b25774
sobolevn <mail@sobolevn.me>
2026-03-15 15:40:35 +0300

    Fix CI

--- tests/test_integration/conftest.py
+++ tests/test_integration/conftest.py
@@ -5,6 +5,9 @@ from dmr.settings import Settings
 
 
 @pytest.fixture(autouse=True)
-def _generate_examples(settings: LazySettings) -> None:
+def _generate_examples(
+    settings: LazySettings,
+    dmr_clean_settings: None,
+) -> None:
     # Our own settings:
     settings.DMR_SETTINGS = {Settings.openapi_examples_seed: 1}
