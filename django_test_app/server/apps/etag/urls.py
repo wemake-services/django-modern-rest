d149b4b75a262783ae20193b2ea5577870aeaf6f
milssky <2furius@gmail.com>
2026-03-23 17:48:45 +0300

    fix examples

--- django_test_app/server/apps/etag/urls.py
+++ django_test_app/server/apps/etag/urls.py
@@ -12,10 +12,9 @@ router = Router(
     ],
 )
 
-# Used by docs runner when `use_urlpatterns: true` is set in `# run` examples.
 urlpatterns = router.urls
 
 
-# run: {"controller": "ConditionalETagController", "method": "get", "url": "/api/etag/1/", "use_urlpatterns": true, "curl_args": ["-D", "-"]}  # noqa: ERA001, E501
-# run: {"controller": "ConditionalETagController", "method": "get", "url": "/api/etag/1/", "use_urlpatterns": true, "headers": {"If-None-Match": "\"user-1-2026-03-23T12:30:00+00:00\""}, "curl_args": ["-D", "-"], "fail-with-body": false}  # noqa: ERA001, E501
-# run: {"controller": "ConditionalETagController", "method": "get", "url": "/api/etag/2/", "use_urlpatterns": true, "curl_args": ["-D", "-"]}  # noqa: ERA001, E501
+# run: {"controller": "ConditionalETagController", "method": "get", "url": "/1/", "use_urlpatterns": true, "curl_args": ["-D", "-"]}  # noqa: ERA001, E501
+# run: {"controller": "ConditionalETagController", "method": "get", "url": "/1/", "use_urlpatterns": true, "headers": {"If-None-Match": "\"user-1-2026-03-23T12:30:00+00:00\""}, "curl_args": ["-D", "-"], "fail-with-body": false}  # noqa: ERA001, E501
+# run: {"controller": "ConditionalETagController", "method": "get", "url": "/2/", "use_urlpatterns": true, "curl_args": ["-D", "-"]}  # noqa: ERA001, E501
