95778363fb4c89d1dfc186ff87fa48bf3a85c511
sobolevn <mail@sobolevn.me>
2026-04-04 22:02:11 +0300

    Support more  platforms for wheels

--- .github/workflows/build-release-matrix.sh
+++ .github/workflows/build-release-matrix.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 {
   cibuildwheel --print-build-identifiers --platform linux \
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
+  && cibuildwheel --print-build-identifiers --platform linux --archs aarch64 \
+  | pyp 'json.dumps({"only": x, "os": "ubuntu-24.04-arm"})' \
   && cibuildwheel --print-build-identifiers --platform macos \
-  | pyp 'json.dumps({"only": x, "os": "macos-latest"})'
+  | pyp 'json.dumps({"only": x, "os": "macos-latest"})' \
+  && cibuildwheel --print-build-identifiers --platform windows \
+  | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
+  && cibuildwheel --print-build-identifiers --platform windows --archs ARM64 \
+  | pyp 'json.dumps({"only": x, "os": "windows-11-arm"})'
 } | pyp 'json.dumps(list(map(json.loads, lines)))' > /tmp/matrix
