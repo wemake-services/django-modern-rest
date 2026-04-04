1fa5d45a23199560b930c6afa1241a4ac22a8820
sobolevn <mail@sobolevn.me>
2026-04-04 22:12:18 +0300

    Support more  platforms for wheels

--- .github/workflows/build-pr-matrix.sh
+++ .github/workflows/build-pr-matrix.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 {
   CIBW_BUILD="cp311-*" cibuildwheel --print-build-identifiers --platform linux \
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
-  CIBW_BUILD="cp312-*" cibuildwheel --print-build-identifiers --platform windows \
+  && CIBW_BUILD="cp312-*" cibuildwheel --print-build-identifiers --platform windows \
   | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
   && CIBW_BUILD="cp314-*" cibuildwheel --print-build-identifiers --platform macos \
   | pyp 'json.dumps({"only": x, "os": "macos-latest"})'
