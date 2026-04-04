7bc8e5f6f2871ac34e5b84b23051e3d94fceca1c
sobolevn <mail@sobolevn.me>
2026-04-04 18:04:00 +0300

    Fix CI

--- .github/workflows/build-release-matrix.sh
+++ .github/workflows/build-release-matrix.sh
@@ -7,8 +7,4 @@ set -euo pipefail
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
   && cibuildwheel --print-build-identifiers --platform macos \
   | pyp 'json.dumps({"only": x, "os": "macos-latest"})' \
-  && cibuildwheel --print-build-identifiers --platform windows \
-  | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
-  && cibuildwheel --print-build-identifiers --platform windows --archs ARM64 \
-  | pyp 'json.dumps({"only": x, "os": "windows-11-arm"})'
 } | pyp 'json.dumps(list(map(json.loads, lines)))' > /tmp/matrix
