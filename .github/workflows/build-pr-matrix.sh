67b149a86b160c3811b035fcf53a7d2302e84189
sobolevn <mail@sobolevn.me>
2026-04-05 09:06:13 +0300

    Also try windows

--- .github/workflows/build-pr-matrix.sh
+++ .github/workflows/build-pr-matrix.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 {
   CIBW_BUILD="cp311-*" cibuildwheel --print-build-identifiers --platform linux \
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
+  && CIBW_BUILD="cp312-*" cibuildwheel --print-build-identifiers --platform windows \
+  | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
   && CIBW_BUILD="cp314-*" cibuildwheel --print-build-identifiers --platform macos \
   | pyp 'json.dumps({"only": x, "os": "macos-latest"})'
 } | pyp 'json.dumps(list(map(json.loads, lines)))' > /tmp/matrix
