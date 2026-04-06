a3112b983b90ef61d0d6a3b491b5ad45759c336e
sobolevn <mail@sobolevn.me>
2026-04-06 20:45:36 +0300

    Decrease the number of jobs in PR CI

--- .github/workflows/build-pr-matrix.sh
+++ .github/workflows/build-pr-matrix.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-export CIBW_ARCHS_LINUX='x86_64'
-export CIBW_SKIP='*-musllinux_*'  # Disable `musllinux` on PRs.
-
 {
-  CIBW_BUILD="cp311-*" cibuildwheel --print-build-identifiers --platform linux \
+  CIBW_ARCHS_LINUX='x86_64' CIBW_SKIP='*-musllinux_*' CIBW_BUILD="cp311-*" \
+    cibuildwheel --print-build-identifiers --platform linux \
   | pyp 'json.dumps({"only": x, "os": "ubuntu-latest"})' \
   && CIBW_BUILD="cp312-*" cibuildwheel --print-build-identifiers --platform windows \
   | pyp 'json.dumps({"only": x, "os": "windows-latest"})' \
