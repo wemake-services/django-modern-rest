a114b419085f24ea1a201e2bc991c761365f67a6
milssky <2furius@gmail.com>
2026-04-12 17:34:00 +0300

    fix: openapi uis

--- dmr/openapi/views/stoplight.py
+++ dmr/openapi/views/stoplight.py
@@ -33,8 +33,8 @@ class StoplightView(OpenAPIView):
             self.template_name,
             context={
                 'title': self.schema.info.title,
-                'schema': (
-                    self.schema.convert(skip_validation=self.skip_validation),
+                'schema': self.schema.convert(
+                    skip_validation=self.skip_validation,
                 ),
                 'stoplight_cdn': cdn_config.get('stoplight'),
             },
