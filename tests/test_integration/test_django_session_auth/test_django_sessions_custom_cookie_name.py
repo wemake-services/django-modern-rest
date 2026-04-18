3028b62bee3f4956fef9797c01ed833e9b11c2bc
sobolevn <mail@sobolevn.me>
2026-04-18 12:15:08 +0300

    Update mypy to 1.20.1

--- tests/test_integration/test_django_session_auth/test_django_sessions_custom_cookie_name.py
+++ tests/test_integration/test_django_session_auth/test_django_sessions_custom_cookie_name.py
@@ -2,9 +2,8 @@ from http import HTTPStatus
 from http.cookies import CookieError
 
 import pytest
-from django.conf import settings
+from django.conf import LazySettings
 from django.contrib.auth.models import User
-from django.test import override_settings
 from django.urls import reverse
 from faker import Faker
 
@@ -29,41 +28,10 @@ def user(faker: Faker, password: str) -> User:
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    'check_url',
-    [
-        reverse('api:django_session_auth:django_session_sync'),
-        reverse('api:django_session_auth:django_session_async'),
-    ],
-)
-def test_django_sessions_default_cookie_name(
-    dmr_client: DMRClient,
-    user: User,
-    password: str,
-    *,
-    check_url: str,
-) -> None:
-    """Ensure that the default session cookie name is used correctly."""
-    response = dmr_client.post(
-        check_url,
-        data={'username': user.username, 'password': password},
-        content_type='application/json',
-    )
-
-    assert response.status_code == HTTPStatus.OK, response.content
-    assert response.headers['Content-Type'] == 'application/json'
-    assert response.cookies[settings.SESSION_COOKIE_NAME]
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
     'cookie_name',
     [
-        'sessionid',
         'dmr_sessions',
-        'django_sessionid',
-        'CustomSessionCookieName',
         'auth-token-id',
-        's',
         'session_manager_id_for_production_env_0123456789',
     ],
 )
@@ -78,22 +46,23 @@ def test_django_sessions_custom_cookie_name(
     dmr_client: DMRClient,
     user: User,
     password: str,
+    settings: LazySettings,
+    *,
     cookie_name: str,
     check_url: str,
 ) -> None:
     """Ensure that custom session cookie names are respected by the API."""
-    default_cookie_name = settings.SESSION_COOKIE_NAME
-    with override_settings(SESSION_COOKIE_NAME=cookie_name):
-        response = dmr_client.post(
-            check_url,
-            data={'username': user.username, 'password': password},
-            content_type='application/json',
-        )
-        assert response.status_code == HTTPStatus.OK, response.content
-        assert response.headers['Content-Type'] == 'application/json'
-        assert response.cookies[cookie_name]
-        if cookie_name != default_cookie_name:
-            assert default_cookie_name not in response.cookies
+    settings.SESSION_COOKIE_NAME = cookie_name
+
+    response = dmr_client.post(
+        check_url,
+        data={'username': user.username, 'password': password},
+        content_type='application/json',
+    )
+
+    assert response.status_code == HTTPStatus.OK, response.content
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response.cookies[cookie_name]
 
 
 @pytest.mark.django_db
@@ -102,8 +71,6 @@ def test_django_sessions_custom_cookie_name(
     [
         'session cookie',
         'session;id',
-        'session=id',
-        'session,id',
         '',
         '   ',
     ],
@@ -119,16 +86,16 @@ def test_django_sessions_wrong_cookie_name(
     dmr_client: DMRClient,
     user: User,
     password: str,
+    settings: LazySettings,
+    *,
     invalid_cookie_name: str,
     check_url: str,
 ) -> None:
     """Ensure that RFC-invalid cookie names raise a CookieError."""
-    with (
-        override_settings(SESSION_COOKIE_NAME=invalid_cookie_name),
-        pytest.raises(
-            CookieError,
-            match=f'Illegal key {invalid_cookie_name!r}',
-        ),
+    settings.SESSION_COOKIE_NAME = invalid_cookie_name
+    with pytest.raises(
+        CookieError,
+        match=f'Illegal key {invalid_cookie_name!r}',
     ):
         dmr_client.post(
             check_url,
