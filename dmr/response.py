3d62c399a9c4fc6e488845e5a92586d87c48f38e
sobolevn <mail@sobolevn.me>
2026-05-07 14:47:39 +0300

    Update dmr/response.py

--- dmr/response.py
+++ dmr/response.py
@@ -10,7 +10,8 @@ from django.utils.encoding import iri_to_uri
 try:
     from django.utils.http import MAX_URL_REDIRECT_LENGTH
 except ImportError:  # pragma: no cover
-    MAX_URL_REDIRECT_LENGTH = 16384  # Django 5.2 compat
+    # Django 5.2.0 compat:
+    MAX_URL_REDIRECT_LENGTH = 16384  # pyright: ignore[reportConstantRedefinition]
 from typing_extensions import TypeVar
 
 from dmr.cookies import NewCookie, set_cookies
