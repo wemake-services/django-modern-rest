be293e4b8f0f2c827c53a8ebe98acd96bd0ff7b8
sobolevn <mail@sobolevn.me>
2026-06-09 18:03:53 +0300

    Update test_admin.py

--- tests/test_unit/test_security/test_jwt/test_blocklist/test_admin.py
+++ tests/test_unit/test_security/test_jwt/test_blocklist/test_admin.py
@@ -1,14 +1,7 @@
-import pytest
 from django.contrib import admin
 
 from dmr.security.jwt.blocklist.admin import BlocklistedJWTokenAdmin
 from dmr.security.jwt.blocklist.models import BlocklistedJWToken
-
-
-@pytest.fixture
-def admin_instance() -> BlocklistedJWTokenAdmin:
-    """Create admin instance for tests."""
-    return BlocklistedJWTokenAdmin(BlocklistedJWToken, admin.site)
 
 
 def test_admin_registered() -> None:
