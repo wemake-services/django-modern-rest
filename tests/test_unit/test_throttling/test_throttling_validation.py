1eb05f49714a6472f8ef3f162e5d98d385e22bd2
azad-mammedov <azadmammedov6@gmail.com>
2026-06-17 00:08:59 +0400

    Fix type-ignore placement for mypy and pyrefly

--- tests/test_unit/test_throttling/test_throttling_validation.py
+++ tests/test_unit/test_throttling/test_throttling_validation.py
@@ -129,14 +129,12 @@ def test_sync_or_async_throttle_not_allowed_at_endpoint_level(  # noqa: WPS118
         class _SyncEndpointController(
             Controller[PydanticSerializer],
         ):
-            @modify(
-                throttling=[
-                    SyncOrAsyncThrottle(  # type: ignore[arg-type]
-                        SyncThrottle(1, Rate.second),
-                        AsyncThrottle(1, Rate.second),
-                    ),
-                ],
-            )
+            @modify(throttling=[  # type: ignore[arg-type]
+                SyncOrAsyncThrottle(
+                    SyncThrottle(1, Rate.second),
+                    AsyncThrottle(1, Rate.second),
+                ),
+            ])
             def get(self) -> str:
                 raise NotImplementedError
 
