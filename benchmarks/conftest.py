bcdc18b6906736f196f6b504ce2dd1c25904011f
sobolevn <mail@sobolevn.me>
2026-08-03 12:47:33 +0300

    Update benchmarks/conftest.py

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def pytest_collection_modifyitems(items):
+def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: list[pytest.Item]) -> None:
     """Automatically disable timeout for all of benchmarks"""
     for item in items:
         item.add_marker(pytest.mark.timeout(0))
