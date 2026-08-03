44f81f1c02a2412041829c243f3b877eafa0da38
Astley <aslbekxamidov2007@gmail.com>
2026-08-03 12:54:28 +0500

    test: disable timeout on test_schemathesis

--- tests/test_integration/test_openapi/test_schema.py
+++ tests/test_integration/test_openapi/test_schema.py
@@ -112,7 +112,7 @@ class _DjangoSessionAuth:
 
 @schema.parametrize()
 @h_settings(max_examples=_MAX_EXAMPLES)
-@pytest.mark.timeout(60)
+@pytest.mark.timeout(0)
 def test_schemathesis(
     tracecov_map: 'tracecov.CoverageMap | None',
     *,
