2e6a5aaead81fae16a940c31d169e579d1573940
sobolevn <mail@sobolevn.me>
2026-08-05 11:32:26 +0300

    Fix CI

--- tests/test_integration/test_throttling/conftest.py
+++ tests/test_integration/test_throttling/conftest.py
@@ -58,15 +58,12 @@ async def redis_async_client(
         pytest.skip(reason='Redis server was not found')
 
 
-def pytest_collection_modifyitems(  # pragma: no cover
+def pytest_collection_modifyitems(
     session: pytest.Session,
     config: pytest.Config,
     items: list[pytest.Item],  # noqa: WPS110
 ) -> None:
     """Automatically run all throttling tests on a single worker."""
     # Otherwise, there can be parallel cache access / cache clear operations.
-    has_xdist = os.environ.get('PYTEST_XDIST_WORKER', '')
-    if not has_xdist:
-        return
     for test_item in items:
         test_item.add_marker(pytest.mark.xdist_group('throttling'))
--- tests/test_unit/test_throttling/conftest.py
+++ tests/test_unit/test_throttling/conftest.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from django.core.cache import cache
 
@@ -9,15 +7,12 @@ def _clean_cache() -> None:
     cache.clear()
 
 
-def pytest_collection_modifyitems(  # pragma: no cover
+def pytest_collection_modifyitems(
     session: pytest.Session,
     config: pytest.Config,
     items: list[pytest.Item],  # noqa: WPS110
 ) -> None:
     """Automatically run all throttling tests on a single worker."""
     # Otherwise, there can be parallel cache access / cache clear operations.
-    has_xdist = os.environ.get('PYTEST_XDIST_WORKER', '')
-    if not has_xdist:
-        return
     for test_item in items:
         test_item.add_marker(pytest.mark.xdist_group('throttling'))
