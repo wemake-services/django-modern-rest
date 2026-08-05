157af3c7e3f4f4020022f6626e791e2143783778
sobolevn <mail@sobolevn.me>
2026-08-05 11:14:51 +0300

    Fix flake8

--- tests/test_integration/test_throttling/conftest.py
+++ tests/test_integration/test_throttling/conftest.py
@@ -61,9 +61,9 @@ async def redis_async_client(
 def pytest_collection_modifyitems(
     session: pytest.Session,
     config: pytest.Config,
-    items: list[pytest.Item],
+    items: list[pytest.Item],  # noqa: WPS110
 ) -> None:
     """Automatically run all throttling tests on a single worker."""
     # Otherwise, there can be parallel cache access / cache clear operations.
-    for item in items:
-        item.add_marker(pytest.mark.xdist_group('throttling'))
+    for test_item in items:
+        test_item.add_marker(pytest.mark.xdist_group('throttling'))
--- tests/test_unit/test_throttling/conftest.py
+++ tests/test_unit/test_throttling/conftest.py
@@ -10,9 +10,9 @@ def _clean_cache() -> None:
 def pytest_collection_modifyitems(
     session: pytest.Session,
     config: pytest.Config,
-    items: list[pytest.Item],
+    items: list[pytest.Item],  # noqa: WPS110
 ) -> None:
     """Automatically run all throttling tests on a single worker."""
     # Otherwise, there can be parallel cache access / cache clear operations.
-    for item in items:
-        item.add_marker(pytest.mark.xdist_group('throttling'))
+    for test_item in items:
+        test_item.add_marker(pytest.mark.xdist_group('throttling'))
