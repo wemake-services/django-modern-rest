382f71fbb1213592fa7a0a1181465efeb46a09b6
sobolevn <mail@sobolevn.me>
2026-08-09 11:59:50 +0300

    Allow to pass multiple components to `OpenAPIConfig` (#1233)

--- tests/test_unit/test_openapi/test_spec.py
+++ tests/test_unit/test_openapi/test_spec.py
@@ -1,5 +1,6 @@
 import pytest
 from django.conf import LazySettings
+from inline_snapshot import snapshot
 
 from dmr.openapi import (
     OpenAPIConfig,
@@ -7,7 +8,14 @@ from dmr.openapi import (
     build_schema,
     default_config,
 )
-from dmr.openapi.objects import Tag
+from dmr.openapi.objects import (
+    Components,
+    Example,
+    Header,
+    OpenAPIType,
+    Schema,
+    Tag,
+)
 from dmr.routing import Router
 
 
@@ -69,3 +77,63 @@ def test_pass_both_context_and_config() -> None:
     context = OpenAPIContext(config)
     with pytest.raises(ValueError, match='Passing both'):
         build_schema(router, context=context, config=config)  # type: ignore[call-overload]
+
+
+def test_multiple_components() -> None:  # noqa: WPS210
+    """Ensures that passing multiple components merges correctly."""
+    components_one = Components(
+        headers={
+            'one': Header(
+                description='First',
+                schema=Schema(type=OpenAPIType.STRING),
+            ),
+        },
+        schemas={'test': Schema(type=OpenAPIType.STRING)},
+    )
+    components_two = Components(
+        headers={
+            'two': Header(
+                description='Second',
+                schema=Schema(type=OpenAPIType.STRING),
+            ),
+        },
+    )
+    components_three = Components(
+        examples={'example-one': Example(summary='an example')},
+    )
+    config = OpenAPIConfig(
+        title='my title',
+        version='1.0.0',
+        components=[components_one, components_two, components_three],
+    )
+    router = Router('', [])
+
+    schema = build_schema(router, config=config)
+
+    assert schema.convert()['components'] == snapshot({
+        'schemas': {'test': {'type': 'string'}},
+        'examples': {'example-one': {'summary': 'an example'}},
+        'headers': {
+            'one': {'schema': {'type': 'string'}, 'description': 'First'},
+            'two': {'schema': {'type': 'string'}, 'description': 'Second'},
+        },
+    })
+
+
+def test_multiple_components_conflict() -> None:
+    """Ensures that passing multiple components conflicts loudly."""
+    components_one = Components(
+        schemas={'test': Schema(type=OpenAPIType.STRING)},
+    )
+    components_two = Components(
+        schemas={'test': Schema(type=OpenAPIType.STRING)},
+    )
+    config = OpenAPIConfig(
+        title='my title',
+        version='1.0.0',
+        components=[components_one, components_two],
+    )
+    router = Router('', [])
+
+    with pytest.raises(ValueError, match=r"with shared keys: \{'test'\}"):
+        build_schema(router, config=config)
