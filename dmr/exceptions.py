a2d44b103a3ce8655db9afdbf456f11eb321fdaa
Mark Paronyan <markparonian@gmail.com>
2026-08-29 15:39:35 +0300

    docs(dmr/exceptions): fix typos (#1319)

--- dmr/exceptions.py
+++ dmr/exceptions.py
@@ -43,7 +43,7 @@ class InternalServerError(Exception):
     Indicates that something is broken on our side.
 
     If ``settings.DEBUG`` is enabled, we share the details: what has happened.
-    If it disabled, we hust show a generic message.
+    If it is disabled, we just show a generic message.
     """
 
     default_message: ClassVar[str | Promise] = _('Internal server error')
