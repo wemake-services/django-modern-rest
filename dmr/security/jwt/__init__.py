e68cfedde94d91feab253c85f552e100dc915fda
Kirill Vedmetskii <kirik@kirill-vedmetskii.ru>
2026-09-01 16:29:41 +0300

    Fix lint config and fix formating

--- dmr/security/jwt/__init__.py
+++ dmr/security/jwt/__init__.py
@@ -6,11 +6,12 @@ try:
     import jwt  # noqa: F401  # pyright: ignore[reportUnusedImport]
 except ImportError:  # pragma: no cover
     print(  # noqa: WPS421
-        "Looks like `pyjwt` is not installed, "
+        'Looks like `pyjwt` is not installed, '
         "consider using `pip install 'django-modern-rest[jwt]'`",
     )
     raise
 
+from dmr.security.jwt.auth.base import request_jwt as request_jwt
 from dmr.security.jwt.auth.cookie import (
     CookieJWTAsyncAuth as CookieJWTAsyncAuth,
 )
@@ -23,5 +24,4 @@ from dmr.security.jwt.auth.header import (
 from dmr.security.jwt.auth.header import HeaderJWTSyncAuth as HeaderJWTSyncAuth
 from dmr.security.jwt.auth.header import JWTAsyncAuth as JWTAsyncAuth
 from dmr.security.jwt.auth.header import JWTSyncAuth as JWTSyncAuth
-from dmr.security.jwt.auth.base import request_jwt as request_jwt
 from dmr.security.jwt.token import JWToken as JWToken
