1181df275dbc2163ee05e1e2ccb4616116d9e6b9
Olga Pichuzhkina <vyhuholl@protonmail.com>
2026-09-03 00:28:06 +0300

    Test the runtime effect of `exclude_validate_responses`, #1370

--- tests/test_unit/test_controllers/test_exclude_validate_responses.py
+++ tests/test_unit/test_controllers/test_exclude_validate_responses.py
@@ -38,59 +38,7 @@ class _PerEndpoint(Controller[PydanticSerializer]):
         exclude_validate_responses=_SERVER_ERROR,
     )
     def post(self) -> HttpResponse:
-        raise NotImplementedError
-
-
-def test_metadata_per_controller() -> None:
-    """Ensure that status codes can be excluded on controller level."""
-    metadata = _PerController.api_endpoints['GET'].metadata
-
-    assert metadata.exclude_validate_responses == _SERVER_ERROR
-
-
-@pytest.mark.parametrize('method', [HTTPMethod.GET, HTTPMethod.POST])
-def test_metadata_per_endpoint(*, method: HTTPMethod) -> None:
-    """Ensure that status codes can be excluded on endpoint level."""
-    metadata = _PerEndpoint.api_endpoints[str(method)].metadata
-
-    assert metadata.exclude_validate_responses == _SERVER_ERROR
-
-
-def test_metadata_per_settings(settings: LazySettings) -> None:
-    """Ensure that status codes can be excluded on settings level."""
-    settings.DMR_SETTINGS = {
-        Settings.exclude_validate_responses: {
-            HTTPStatus.INTERNAL_SERVER_ERROR,
-        },
-    }
-
-    class _PerSettings(Controller[PydanticSerializer]):
-        def get(self) -> str:
-            raise NotImplementedError
-
-    metadata = _PerSettings.api_endpoints['GET'].metadata
-
-    assert metadata.exclude_validate_responses == _SERVER_ERROR
-
-
-def test_metadata_overrides(settings: LazySettings) -> None:
-    """Ensure that excluded status codes can be reset back."""
-    settings.DMR_SETTINGS = {
-        Settings.exclude_validate_responses: {
-            HTTPStatus.INTERNAL_SERVER_ERROR,
-        },
-    }
-
-    class _PerSettings(Controller[PydanticSerializer]):
-        exclude_validate_responses = frozenset((HTTPStatus.NOT_FOUND,))
-
-        @modify(exclude_validate_responses=None)
-        def get(self) -> str:
-            raise NotImplementedError
-
-    metadata = _PerSettings.api_endpoints['GET'].metadata
-
-    assert metadata.exclude_validate_responses == frozenset()
+        raise InternalServerError('database is down')
 
 
 def test_not_excluded_status_code(dmr_rf: DMRRequestFactory) -> None:
@@ -104,24 +52,109 @@ def test_not_excluded_status_code(dmr_rf: DMRRequestFactory) -> None:
     assert 'is not specified in the list' in str(response.content)
 
 
-@pytest.mark.parametrize(
-    'controller',
-    [_PerController, _PerEndpoint],
-)
-def test_excluded_status_code(
-    dmr_rf: DMRRequestFactory,
-    controller: type[Controller[PydanticSerializer]],
-) -> None:
-    """Ensure that excluded status codes are not validated."""
+def test_per_controller(dmr_rf: DMRRequestFactory) -> None:
+    """Ensure that status codes can be excluded on controller level."""
     request = dmr_rf.get('/whatever/')
+    metadata = _PerController.api_endpoints['GET'].metadata
 
-    response = controller.as_view()(request)
+    response = _PerController.as_view()(request)
 
+    assert metadata.exclude_validate_responses == _SERVER_ERROR
     assert isinstance(response, HttpResponse)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
     assert json.loads(response.content) == {
         'detail': [{'msg': 'Internal server error'}],
     }
+
+
+@pytest.mark.parametrize('method', [HTTPMethod.GET, HTTPMethod.POST])
+def test_per_endpoint(
+    dmr_rf: DMRRequestFactory,
+    *,
+    method: HTTPMethod,
+) -> None:
+    """Ensure that status codes can be excluded on endpoint level."""
+    request = dmr_rf.generic(str(method), '/whatever/', data=None)
+    metadata = _PerEndpoint.api_endpoints[str(method)].metadata
+
+    response = _PerEndpoint.as_view()(request)
+
+    assert metadata.exclude_validate_responses == _SERVER_ERROR
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+    assert json.loads(response.content) == {
+        'detail': [{'msg': 'Internal server error'}],
+    }
+
+
+def test_per_settings(
+    dmr_rf: DMRRequestFactory,
+    settings: LazySettings,
+) -> None:
+    """Ensure that status codes can be excluded on settings level."""
+    settings.DMR_SETTINGS = {
+        Settings.exclude_validate_responses: {
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+        },
+    }
+
+    class _PerSettings(Controller[PydanticSerializer]):
+        def get(self) -> str:
+            raise InternalServerError('database is down')
+
+    request = dmr_rf.get('/whatever/')
+    metadata = _PerSettings.api_endpoints['GET'].metadata
+
+    response = _PerSettings.as_view()(request)
+
+    assert metadata.exclude_validate_responses == _SERVER_ERROR
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+def test_overrides(
+    dmr_rf: DMRRequestFactory,
+    settings: LazySettings,
+) -> None:
+    """Ensure that excluded status codes can be reset back."""
+    settings.DMR_SETTINGS = {
+        Settings.exclude_validate_responses: {
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+        },
+    }
+
+    class _PerSettings(Controller[PydanticSerializer]):
+        exclude_validate_responses = frozenset((HTTPStatus.NOT_FOUND,))
+
+        @modify(exclude_validate_responses=None)
+        def get(self) -> str:
+            raise InternalServerError('database is down')
+
+    request = dmr_rf.get('/whatever/')
+    metadata = _PerSettings.api_endpoints['GET'].metadata
+
+    response = _PerSettings.as_view()(request)
+
+    assert metadata.exclude_validate_responses == frozenset()
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+class _StillValidated(Controller[PydanticSerializer]):
+    exclude_validate_responses = _SERVER_ERROR
+
+    def get(self) -> list[str]:
+        return [1, 2]  # type: ignore[list-item]
+
+
+def test_other_status_codes_are_validated(dmr_rf: DMRRequestFactory) -> None:
+    """Ensure that excluding one status code does not disable the rest."""
+    request = dmr_rf.get('/whatever/')
+
+    response = _StillValidated.as_view()(request)
+
+    assert isinstance(response, HttpResponse)
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
 class _WronglyDescribed(Controller[PydanticSerializer]):
