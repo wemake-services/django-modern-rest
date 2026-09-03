43f056bf0253849d0a293254b3da5768f3ccdd0d
sobolevn <mail@sobolevn.me>
2026-09-03 12:06:57 +0300

    Fix some re-exports

--- dmr/security/jwt/auth/__init__.py
+++ dmr/security/jwt/auth/__init__.py
@@ -2,6 +2,10 @@
 # before `dmr@0.15.0`, `jwt.auth` was a module, not a package.
 from dmr.security.jwt.auth.base import request_jwt as request_jwt
 from dmr.security.jwt.auth.base import set_request_attrs as set_request_attrs
+from dmr.security.jwt.auth.cookie import (
+    CookieJWTAsyncAuth as CookieJWTAsyncAuth,
+)
+from dmr.security.jwt.auth.cookie import CookieJWTSyncAuth as CookieJWTSyncAuth
 from dmr.security.jwt.auth.header import (
     HeaderJWTAsyncAuth as HeaderJWTAsyncAuth,
 )
