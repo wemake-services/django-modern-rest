184d9310a5461e5bc2aba13195bf3dbf614de9ef
Astley <aslbekxamidov2007@gmail.com>
2026-09-09 12:38:19 +0500

    manifest error test

--- django_test_app/server/settings.py
+++ django_test_app/server/settings.py
@@ -160,7 +160,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 STORAGES = {
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 # Default primary key field type
