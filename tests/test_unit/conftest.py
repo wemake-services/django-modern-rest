59829890a261d80c88ab04119648e044bcfca8df
Abduaziz <mail@ziyodov.uz>
2026-09-09 20:13:35 +0500

    refactor: simplify `assert_csrf_failure_message`
    Co-authored-by: sobolevn <mail@sobolevn.me>

--- tests/test_unit/conftest.py
+++ tests/test_unit/conftest.py
@@ -10,20 +10,13 @@ from inline_snapshot import snapshot
 CsrfFailureAssertion: TypeAlias = Callable[[HttpResponse], None]
 
 
-@pytest.fixture(
-    params=[True, False],
-    ids=['debug_on', 'debug_off'],
-)
 def assert_csrf_failure_message(
     settings: LazySettings,
-    request: pytest.FixtureRequest,
 ) -> CsrfFailureAssertion:
     """Assert CSRF failure message according to debug mode setting."""
-    is_debug_mode_active = request.param
-    settings.DEBUG = is_debug_mode_active
 
-    def _assert(response: HttpResponse) -> None:
-        if is_debug_mode_active:
+    def factory(response: HttpResponse) -> None:
+        if settings.DEBUG:
             assert json.loads(response.content) == snapshot({
                 'detail': [{'msg': 'CSRF Failed: CSRF cookie not set.'}],
             })
