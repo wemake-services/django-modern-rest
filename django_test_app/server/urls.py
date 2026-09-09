a440021d4377fb66ed0fb2dfead8af5ab8de8f4a
Astley <aslbekxamidov2007@gmail.com>
2026-09-09 21:31:19 +0500

    added description for user/password

--- django_test_app/server/urls.py
+++ django_test_app/server/urls.py
@@ -55,6 +55,11 @@ openapi_config = OpenAPIConfig(
     description=(
         'Test application showcasing core functionality of the framework. '
         'Не АСКИИ текст'  # noqa: RUF001
+        """
+        ### Demo Credentials
+        - **Username:** `admin`
+        - **Password:** `password`
+        """
     ),
     terms_of_service='Usage is intended for testing purposes only.',
     contact=Contact(name='Core Developer', email='mail@sobolevn.me'),
