f2f2a51a4d2d0a72329ee3ece0c6b94b98f250d0
Pavel Kirilin <s3riussan@gmail.com>
2026-09-10 01:23:52 +0200

    Initial cursor pagination implementation.
    Signed-off-by: Pavel Kirilin <s3riussan@gmail.com>

---
+++ dmr/pagination/__init__.py
@@ -0,0 +1,5 @@
+from dmr.pagination.base import Page as Page
+from dmr.pagination.base import Paginated as Paginated
+from dmr.pagination.cursor import CursorPage as CursorPage
+from dmr.pagination.cursor import CursorPaginated as CursorPaginated
+from dmr.pagination.cursor import CursorPaginator as CursorPaginator
