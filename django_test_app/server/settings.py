95938325029808146ad7d66d2a45b27f7ee5b43b
Astley <aslbekxamidov2007@gmail.com>
2026-09-09 12:09:06 +0500

    quick fix for whitenoise failure

--- django_test_app/server/settings.py
+++ django_test_app/server/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 STORAGES = {
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
