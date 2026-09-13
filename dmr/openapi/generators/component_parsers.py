9ce0fed27595c096673c4c5610ab5032e4172538
sobolevn <mail@sobolevn.me>
2026-09-13 14:36:06 +0300

    Update component_parsers.py

--- dmr/openapi/generators/component_parsers.py
+++ dmr/openapi/generators/component_parsers.py
@@ -193,11 +193,12 @@ class ComponentParserGenerator:
         return RequestBody(
             content=new_content,
             description=(
-                (schema.description or '')
-                + ' '
-                + (new_schema.description or '')
-            ).strip()
-            or None,
+                (
+                    (schema.description or '')
+                    + ' '
+                    + (new_schema.description or '')
+                ).strip() or None
+            ),
             required=schema.required or new_schema.required,
         )
 
