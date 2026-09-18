2e03bb46b33d9cac7406d9ded6cd3a4d91e72d9f
sobolevn <mail@sobolevn.me>
2026-09-18 20:43:44 +0300

    Fix CI

--- tests/test_unit/test_throttling/test_throttling_types.py
+++ tests/test_unit/test_throttling/test_throttling_types.py
@@ -63,7 +63,7 @@ def test_throttle_replace_empty(
     reason='`copy.replace` is added in Python 3.13',
 )
 @pytest.mark.parametrize('throttle_cls', [SyncThrottle, AsyncThrottle])
-def test_throttle_copy_replace(
+def test_throttle_copy_replace(  # pragma: no cover
     *,
     throttle_cls: type[SyncThrottle | AsyncThrottle],
 ) -> None:
