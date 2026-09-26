f30a6c48beff12d17e0ae014f8d7cf3b8c174832
Alexander Kondratev <kondratev.av@inbox.ru>
2026-09-26 21:32:56 +0500

    simplify runtime type narrowing

--- tests/test_unit/test_internal/test_io/test_aiter_to_iter.py
+++ tests/test_unit/test_internal/test_io/test_aiter_to_iter.py
@@ -19,11 +19,28 @@ async def _complex_events() -> AsyncIterator[str]:
     return  # noqa: WPS324
 
 
+class _NonClosableEvents:
+    """Minimal async iterator which has no ``aclose`` method."""
+
+    def __init__(self) -> None:
+        self._events = iter(('first', 'second'))
+
+    def __aiter__(self) -> AsyncIterator[str]:
+        return self
+
+    async def __anext__(self) -> str:
+        try:
+            return next(self._events)
+        except StopIteration as exc:
+            raise StopAsyncIteration from exc
+
+
 @pytest.mark.parametrize(
     'aiterator',
     [
         _simple_events(),
         _complex_events(),
+        _NonClosableEvents(),
     ],
 )
 def test_aiter_to_iter(
@@ -31,5 +48,5 @@ def test_aiter_to_iter(
     aiterator: AsyncIterator[str],
 ) -> None:
     """Ensure that iterator converter works."""
-    sync = aiter_to_iter(aiterator)
+    sync = aiter_to_iter(aiter(aiterator))
     assert list(sync) == ['first', 'second']
